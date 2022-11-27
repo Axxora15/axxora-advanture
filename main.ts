e535db7166fff324e9dc6c06bdfbb50e3453fa53
Axxora15 <118996317+Axxora15@users.noreply.github.com>
2022-11-27 12:47:23 +0530

    i add 1 more  level with hearts

--- main.ts
+++ main.ts
@@ -1,27 +1,319 @@
 namespace SpriteKind {
     export const flower = SpriteKind.create()
+    export const heart = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     game.over(false, effects.splatter)
 })
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile1`, function (sprite, location) {
-    game.over(true, effects.hearts)
+    level += 1
+    startovaer()
+    game.showLongText("itls me your princess is in another level", DialogLayout.Top)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.heart, function (sprite, otherSprite) {
+    info.changeLifeBy(1)
+    otherSprite.destroy()
+    tiles.placeOnRandomTile(heart, assets.tile`myTile5`)
 })
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Axxora.vy == 0) {
         Axxora.vy = -150
     }
 })
+function startovaer () {
+    Axxora = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . 2 . . . . . . . . 
+        . . . . . . 2 2 2 . . . . . . . 
+        . . . . . 2 2 2 2 2 . . . . . . 
+        . . . . 2 2 2 2 2 2 2 . . . . . 
+        . . . 2 2 2 2 2 2 2 2 2 . . . . 
+        . . . . . . f f 8 f f . . . . . 
+        . . . . . d f 8 2 8 f d . . . . 
+        . . . . . . f 8 8 8 f . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . . . 7 f . f 7 . . . . . 
+        . . . . . . . f . f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Player)
+    controller.moveSprite(Axxora, 100, 0)
+    if (level == 0) {
+        tiles.setCurrentTilemap(tilemap`level1`)
+    } else {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    }
+    tiles.placeOnRandomTile(Axxora, assets.tile`myTile4`)
+    for (let value of tiles.getTilesByType(assets.tile`myTile4`)) {
+        tiles.setTileAt(value, assets.tile`transparency16`)
+    }
+    Axxora.ay = 350
+    scene.cameraFollowSprite(Axxora)
+    info.setLife(5)
+    for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
+        flower = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 3 . . . 3 . . . . . . . 
+            . . . . 3 3 . 3 3 . . . . . . . 
+            . . . . 3 3 7 3 3 . . . . . . . 
+            . . . . . 3 3 3 . . . . . . . . 
+            . . . . . . 7 . . 7 . . . . . . 
+            . . . . 7 . 7 . 7 . . . . . . . 
+            . . . . . 7 7 7 . . . . . . . . 
+            . . . . . . 7 . . . . . . . . . 
+            . . . . . . 7 . . . . . . . . . 
+            . . . . e e e e . . . . . . . . 
+            . . . e e 3 7 e e . . . . . . . 
+            `, SpriteKind.flower)
+        tiles.placeOnTile(flower, value)
+        tiles.setTileAt(value, assets.tile`transparency16`)
+        for (let value of tiles.getTilesByType(assets.tile`myTile5`)) {
+            heart = sprites.create(img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `, SpriteKind.heart)
+            animation.runImageAnimation(
+            heart,
+            [img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 . . . . 2 2 . . . . . 
+                . . 2 2 2 2 . . 2 2 2 2 . . . . 
+                . 2 2 2 3 2 2 2 2 2 2 2 2 . . . 
+                . 2 2 3 2 2 2 2 2 2 2 2 2 . . . 
+                . 2 3 2 2 2 2 2 2 2 2 2 2 . . . 
+                . . 2 3 2 2 2 2 2 2 2 2 . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 . . . . 2 2 . . . . . 
+                . . . 2 2 2 . . 2 2 2 . . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . 3 2 2 2 2 2 2 2 . . . . . 
+                . . . 2 2 2 2 2 2 2 2 . . . . . 
+                . . . 3 2 2 2 2 2 2 2 . . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . 2 . . . . 2 . . . . . . 
+                . . . . 2 2 . . 2 2 . . . . . . 
+                . . . . 3 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 3 2 2 2 2 2 . . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . 2 . . 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 3 2 2 2 . . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 3 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . 3 2 . . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . 2 . . 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 2 2 2 2 . . . . . . . 
+                . . . . . 3 2 2 2 . . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . 2 . . . . 2 . . . . . . 
+                . . . . 2 2 . . 2 2 . . . . . . 
+                . . . . 3 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 2 2 2 2 2 2 . . . . . . 
+                . . . . 3 2 2 2 2 2 . . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 . . . . 2 2 . . . . . 
+                . . . 2 2 2 . . 2 2 2 . . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . 3 2 2 2 2 2 2 2 . . . . . 
+                . . . 2 2 2 2 2 2 2 2 . . . . . 
+                . . . 3 2 2 2 2 2 2 2 . . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 . . . . 2 2 . . . . . 
+                . . 2 2 2 2 . . 2 2 2 2 . . . . 
+                . . 2 2 3 2 2 2 2 2 2 2 . . . . 
+                . . 2 3 2 2 2 2 2 2 2 2 . . . . 
+                . . 3 2 2 2 2 2 2 2 2 2 . . . . 
+                . . 2 3 2 2 2 2 2 2 2 2 . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `,img`
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . 2 2 . . . . 2 2 . . . . . 
+                . . 2 2 2 2 . . 2 2 2 2 . . . . 
+                . 2 2 2 3 2 2 2 2 2 2 2 2 . . . 
+                2 2 2 3 2 2 2 2 2 2 2 2 2 2 . . 
+                . 2 3 2 2 2 2 2 2 2 2 2 2 . . . 
+                . . 2 3 2 2 2 2 2 2 2 2 . . . . 
+                . . . 2 3 2 2 2 2 2 2 . . . . . 
+                . . . . 2 3 2 2 2 2 . . . . . . 
+                . . . . . 2 3 2 2 . . . . . . . 
+                . . . . . . 2 2 . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                . . . . . . . . . . . . . . . . 
+                `],
+            100,
+            true
+            )
+            tiles.placeOnTile(heart, value)
+        }
+    }
+}
 sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
-    sprite.destroy()
     if (Axxora.y < sprite.y) {
+        sprite.destroy()
         info.changeScoreBy(3)
     } else {
         info.changeLifeBy(-1)
         info.changeScoreBy(-3)
-    }
-    if (info.score() == 15) {
-        info.changeLifeBy(1)
+        sprite.destroy()
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.flower, function (sprite, otherSprite) {
@@ -124,52 +416,13 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.flower, function (sprite, otherS
 let bee: Sprite = null
 let flower: Sprite = null
 let Axxora: Sprite = null
+let heart: Sprite = null
+let level = 0
+game.showLongText("once upon a time there is an kid ,his name is axxora ,axxora is a friend of princess , one day there come some kidnapper from another kingdom and you know what they do", DialogLayout.Full)
 scene.setBackgroundColor(9)
-Axxora = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 2 . . . . . . . . 
-    . . . . . . 2 2 2 . . . . . . . 
-    . . . . . 2 2 2 2 2 . . . . . . 
-    . . . . 2 2 2 2 2 2 2 . . . . . 
-    . . . 2 2 2 2 2 2 2 2 2 . . . . 
-    . . . . . . f f 8 f f . . . . . 
-    . . . . . d f 8 2 8 f d . . . . 
-    . . . . . . f 8 8 8 f . . . . . 
-    . . . . . . f f f f f . . . . . 
-    . . . . . . 7 f . f 7 . . . . . 
-    . . . . . . . f . f . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(Axxora, 100, 0)
-tiles.setCurrentTilemap(tilemap`level1`)
-Axxora.ay = 350
-scene.cameraFollowSprite(Axxora)
-info.setLife(5)
-for (let value of tiles.getTilesByType(assets.tile`myTile2`)) {
-    flower = sprites.create(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 3 . . . 3 . . . . . . . 
-        . . . . 3 3 . 3 3 . . . . . . . 
-        . . . . 3 3 7 3 3 . . . . . . . 
-        . . . . . 3 3 3 . . . . . . . . 
-        . . . . . . 7 . . 7 . . . . . . 
-        . . . . 7 . 7 . 7 . . . . . . . 
-        . . . . . 7 7 7 . . . . . . . . 
-        . . . . . . 7 . . . . . . . . . 
-        . . . . . . 7 . . . . . . . . . 
-        . . . . e e e e . . . . . . . . 
-        . . . e e 3 7 e e . . . . . . . 
-        `, SpriteKind.flower)
-    tiles.placeOnTile(flower, value)
-    tiles.setTileAt(value, assets.tile`transparency16`)
-}
+scene.setBackgroundImage(assets.image`mountains`)
+level = 0
+startovaer()
 game.onUpdate(function () {
     if (Axxora.vy < 0) {
         Axxora.setImage(img`
@@ -212,7 +465,7 @@ game.onUpdate(function () {
         if (Axxora.vx < 0) {
             Axxora.image.flipX()
         }
-    } else if (Axxora.x % 2 != 0) {
+    } else if (Axxora.x % 1 != 0) {
         Axxora.setImage(img`
             . . . . . . . . . . . . . . . . 
             . . . . . . . . . . . . . . . . 
